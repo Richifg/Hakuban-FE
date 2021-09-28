4b191c70f4e4822a059d04a3e0a15076cbdc6751
Ricardo Fernandez <richifg@gmail.com>
2021-09-28 11:10:57 -0600

    added prettier config to linter

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,4 @@ import App from './App/App';
 import TestChat from './TestChat/TestChat';
 import TestConnection from './TestConnection/TestConnection';
 
-export {
-    App,
-    TestChat,
-    TestConnection,
-};
+export { App, TestChat, TestConnection };
