08ab4e03b85839e5443b4847a7b0c352f01d37a3
Ricardo Fernandez <richifg@gmail.com>
2022-02-23 19:26:31 -0600

    added dynamic size to grid

--- src/CanvasManager/drawGrid.ts
+++ src/CanvasManager/drawGrid.ts
@@ -1,26 +1,41 @@
 import { CanvasTransform, CanvasSize } from '../interfaces';
 
-const GRID_SIZE = 100; //px
+const INIT_GRID_SIZE = 100; //px
+const MIN_GRID_RENDER_SIZE = 25; //px
+
 function drawGrid(transform: CanvasTransform, size: CanvasSize, ctx: CanvasRenderingContext2D): void {
     const { width, height } = size;
     const { scale, dX, dY } = transform;
 
-    const [initX, initY] = [-GRID_SIZE - dX / scale, -GRID_SIZE - dY / scale];
-    const [endX, endY] = [GRID_SIZE + (width - dX) / scale, GRID_SIZE + (height - dY) / scale];
-    const [offsetX, offsetY] = [(dX / scale) % GRID_SIZE, (dY / scale) % GRID_SIZE];
+    let smallGridSize = INIT_GRID_SIZE;
+    while (smallGridSize * scale < MIN_GRID_RENDER_SIZE) {
+        smallGridSize *= 5;
+    }
 
-    ctx.restore();
+    const [translateToScaleX, translateToScaleY] = [dX / scale, dY / scale];
+    const [widthToScale, heightToScale] = [(width - dX) / scale, (height - dY) / scale];
+
+    ctx.save();
     ctx.beginPath();
     ctx.strokeStyle = '#f0f0f0';
-    for (let x = initX + offsetX; x < endX; x += GRID_SIZE) {
-        ctx.moveTo(x, initY);
-        ctx.lineTo(x, endY);
-    }
-    for (let y = initY + offsetY; y < endY; y += GRID_SIZE) {
-        ctx.moveTo(initX, y);
-        ctx.lineTo(endX, y);
-    }
+
+    // draw grid lines for small and big grid
+    [smallGridSize, smallGridSize * 5].forEach((gridSize) => {
+        const [offsetX, offsetY] = [translateToScaleX % gridSize, translateToScaleY % gridSize];
+        const [initX, initY] = [offsetX - translateToScaleX - gridSize, offsetY - translateToScaleY - gridSize];
+        const [endX, endY] = [widthToScale + gridSize, heightToScale + gridSize];
+
+        for (let x = initX; x < endX; x += gridSize) {
+            ctx.moveTo(x, initY);
+            ctx.lineTo(x, endY);
+        }
+        for (let y = initY; y < endY; y += gridSize) {
+            ctx.moveTo(initX, y);
+            ctx.lineTo(endX, y);
+        }
+    });
     ctx.stroke();
+    ctx.restore();
 }
 
 export default drawGrid;
