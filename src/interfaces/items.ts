a54d855d885dcf88a877b3733092e06a2ce12b26
Ricardo Fernandez <richifg@gmail.com>
2022-03-10 11:05:00 -0600

    reordered interfaces

--- src/interfaces/items.ts
+++ src/interfaces/items.ts
@@ -1,13 +1,25 @@
 export type Point = 'P0' | 'P1' | 'P2' | 'P3';
 
+export type LineConnections = [point: 'P0' | 'P2', x: number, y: number][];
+
 export type BoardItemType = 'text' | 'shape' | 'note' | 'drawing' | 'line';
+
+export interface Coordinates {
+    x0: number;
+    y0: number;
+    x2: number;
+    y2: number;
+}
+
 type ItemType = BoardItemType | 'chat';
+
 interface ItemBase {
     type: ItemType;
     id: string;
     creationDate?: Date;
 }
 
+// --TEXT
 export type Align = 'start' | 'center' | 'end';
 export interface TextStyle {
     fontSize: number;
@@ -19,57 +31,22 @@ export interface TextStyle {
     bold?: boolean;
     skipRendering?: boolean;
 }
-
-export interface ShapeStyle {
-    lineWidth: number;
-    lineColor: string;
-    fillColor: string;
-}
-
-export interface NoteStyle {
-    color: string;
-    size: number;
-}
-
-export interface DrawingStyle {
-    color: string;
-    width: number;
-}
-
-export type ArrowStyle = 'none' | 'simple' | 'narrow' | 'filledTriangle' | 'emptyTriangle';
-export interface LineStyle {
-    color: string;
-    width: number;
-    arrow0Style: ArrowStyle;
-    arrow2Style: ArrowStyle;
-}
-
-export interface Coordinates {
-    x0: number;
-    y0: number;
-    x2: number;
-    y2: number;
-}
-
-export type LineConnections = [point: 'P0' | 'P2', x: number, y: number][];
-
 export interface TextData extends TextStyle {
     content: string;
 }
-
 export interface Text extends ItemBase, Coordinates {
     type: 'text';
     text: TextData;
     connections?: LineConnections;
 }
 
-export interface Note extends ItemBase, Coordinates, NoteStyle {
-    type: 'note';
-    text?: TextData;
-    connections?: LineConnections;
-}
-
+// --SHAPE
 export type ShapeType = 'rect' | 'circle' | 'roundedRect' | 'romboid' | 'triangle' | 'bubble';
+export interface ShapeStyle {
+    lineWidth: number;
+    lineColor: string;
+    fillColor: string;
+}
 export interface Shape extends ItemBase, ShapeStyle, Coordinates {
     type: 'shape';
     shapeType: ShapeType;
@@ -77,6 +54,22 @@ export interface Shape extends ItemBase, ShapeStyle, Coordinates {
     connections?: LineConnections;
 }
 
+// --NOTE
+export interface NoteStyle {
+    color: string;
+    size: number;
+}
+export interface Note extends ItemBase, Coordinates, NoteStyle {
+    type: 'note';
+    text?: TextData;
+    connections?: LineConnections;
+}
+
+// --DRAWING
+export interface DrawingStyle {
+    color: string;
+    width: number;
+}
 export interface Drawing extends ItemBase, Coordinates, DrawingStyle {
     type: 'drawing';
     points: [number, number][];
@@ -84,16 +77,27 @@ export interface Drawing extends ItemBase, Coordinates, DrawingStyle {
     connections?: LineConnections;
 }
 
+// --LINE
+export type ArrowStyle = 'none' | 'simple' | 'narrow' | 'filledTriangle' | 'emptyTriangle';
+export interface LineStyle {
+    color: string;
+    width: number;
+    arrow0Style: ArrowStyle;
+    arrow2Style: ArrowStyle;
+}
 export interface Line extends ItemBase, Coordinates, LineStyle {
     type: 'line';
 }
 
+// --CHAT
 export interface ChatMessage extends ItemBase {
     type: 'chat';
     content: string;
     from: string;
 }
 
-// Items created by users
+// items drawn on the board with coordinates
 export type BoardItem = Note | Text | Shape | Drawing;
+
+// general Item type for any item that users can create
 export type Item = BoardItem | ChatMessage;
