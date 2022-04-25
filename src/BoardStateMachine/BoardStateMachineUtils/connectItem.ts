d2e6afa8e94f35b0a09a6a15eeb099ffcdbb8ba2
Ricardo Fernandez <richifg@gmail.com>
2022-04-25 15:27:40 -0600

    fixed line disconnect when transfering connection

--- src/BoardStateMachine/BoardStateMachineUtils/connectItem.ts
+++ src/BoardStateMachine/BoardStateMachineUtils/connectItem.ts
@@ -2,11 +2,16 @@ import { store } from '../../store/store';
 import { BoardItem, Line, MainPoint } from '../../interfaces';
 import { addItem, addLineConnection } from '../../store/slices/itemsSlice';
 import { isConnectableItem } from '../../utils';
+import { disconnectItem } from './';
 
 // adds a Line connection to an item and returns the updated item
 
 function connectItem(itemToConnect: BoardItem, line: Line, point: MainPoint, boardX: number, boardY: number): void {
     if (isConnectableItem(itemToConnect)) {
+        // line may have a previous connection which needs to be removed
+        disconnectItem(line, point);
+
+        // get relative connection coordinate
         const connectedItem = { ...itemToConnect };
         if (!connectedItem.connections) connectedItem.connections = [];
         const { x0, y0, x2, y2 } = connectedItem;
@@ -14,6 +19,7 @@ function connectItem(itemToConnect: BoardItem, line: Line, point: MainPoint, boa
         const [width, height] = [Math.abs(x2 - x0), Math.abs(y2 - y0)];
         const [x, y] = [parseFloat(((boardX - minX) / width).toFixed(2)), parseFloat(((boardY - minY) / height).toFixed(2))];
         connectedItem.connections = [...connectedItem.connections, [line.id, point, x, y]];
+
         store.dispatch(addItem(connectedItem));
         store.dispatch(addLineConnection({ lineId: line.id, point, itemId: connectedItem.id }));
     }
