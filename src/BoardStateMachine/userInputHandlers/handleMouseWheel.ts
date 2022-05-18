3e45e7fa967c9aaaf885132a8e11bae334362af2
Ricardo Fernandez <richifg@gmail.com>
2022-05-18 15:13:20 -0600

    smoothed wheel scroll

--- src/BoardStateMachine/userInputHandlers/handleMouseWheel.ts
+++ src/BoardStateMachine/userInputHandlers/handleMouseWheel.ts
@@ -7,19 +7,40 @@ import { store } from '../../store/store';
 function handleMouseWheel(e: WheelEvent<HTMLDivElement>): void {
     const { dispatch, getState } = store;
     const { canvasTransform, currentAction } = getState().board;
+    const { scale } = canvasTransform;
 
-    if (currentAction !== 'IDLE') dispatch(setCurrentAction('IDLE'));
+    if (currentAction === 'SLIDE') dispatch(setCurrentAction('IDLE'));
 
-    // calculate new scale, clamped between 4% and 400%
-    const scale = Math.min(Math.max(canvasTransform.scale * (1 - Math.round(e.deltaY) * 0.001), 0.04), 4);
-    dispatch(setCanvasScale(scale));
+    const zoomDelta = Math.abs(e.deltaY * 0.001);
+    const zoomDirection = -1 * Math.sign(e.deltaY);
+
+    let deltaModifier = 1;
+    // changing zoom when zoomed out requires slower rate of zoom
+    if (scale < 0.1) deltaModifier = 0.05;
+    else if (scale < 0.25) deltaModifier = 0.2;
+    else if (scale < 0.5) deltaModifier = 0.4;
+    else if (scale < 0.75) deltaModifier = 0.8;
+    // while changing zoom when zoomed in requires faster rate of zoom
+    else if (scale > 3) deltaModifier = 2.5;
+    else if (scale > 2) deltaModifier = 1.75;
+    else if (scale > 1.75) deltaModifier = 1.5;
+    else if (scale > 1.25) deltaModifier = 1.25;
+    const unclampedScale = scale + zoomDelta * deltaModifier * zoomDirection;
+
+    // calculate new scale, clamped between 2% and 500%
+    let newScale = parseFloat(Math.min(Math.max(unclampedScale, 0.02), 5).toFixed(2));
+
+    // when zoomed out, make sure to zoom at least 1% per scroll
+    if (newScale === scale && scale < 1 && zoomDirection > 0) newScale += 0.01;
+
+    dispatch(setCanvasScale(newScale));
 
     // get cursor board coordinates
     const [cursorX, cursorY] = [e.clientX, e.clientY];
     const [boardX, boardY] = getBoardCoordinates(cursorX, cursorY, canvasTransform);
 
-    // position coordinates on canvas using the new scale
-    const [newX, newY] = getCanvasCoordinates(boardX, boardY, { ...canvasTransform, scale });
+    // position coordinates on canvas using the newScale
+    const [newX, newY] = getCanvasCoordinates(boardX, boardY, { ...canvasTransform, scale: newScale });
 
     // translate so the cursor stays on same position on screen
     dispatch(translateCanvas([cursorX - newX, cursorY - newY]));
